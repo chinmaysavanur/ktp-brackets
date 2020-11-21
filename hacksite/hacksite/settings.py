0e291ec61fcce08b4388ea658a53359a0c4f837b
shanjng <shanjng@umich.edu>
2020-11-21 12:10:53 -0500

    changed back db engine to sqlite3 for now cause you need other downloads

--- hacksite/hacksite/settings.py
+++ hacksite/hacksite/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'hacksite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
