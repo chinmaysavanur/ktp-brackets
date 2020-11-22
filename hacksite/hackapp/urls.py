9413d6f3056a3938b498d93cd1315e2ed1d556de
shanjng <shanjng@umich.edu>
2020-11-22 12:54:53 -0500

    flow of view tourneys, view bracket and/or signup

--- hacksite/hackapp/urls.py
+++ hacksite/hackapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'hackapp' # in case we have another app that also has a path called signup, etc, to help differentiate, used in our index.html file when we do hackapp:signup to route
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('signup/', views.signup, name="signup"),
+    path('bracket/<str:tournament_url>/', views.bracket, name="bracket")
 ]
--- hacksite/hackapp/views.py
+++ hacksite/hackapp/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+import requests
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world.")
+    response = requests.get(f'https://api.challonge.com/v1/tournaments.json?api_key={ settings.API_KEY }')
+    tournaments = response.json()
+    tournamentsList = []
+
+    for tournament in tournaments: 
+        tournamentsList.append({
+                "name": tournament['tournament']['name'],
+                "url": tournament['tournament']['url']
+            })
+
+    context = { 'data': tournamentsList }
+
+    return render(request, 'hackapp/index.html', context)
+
+def signup(request):
+    return render(request, 'hackapp/signup.html')
+
+def bracket(request, tournament_url): 
+    context = { "tournament_url": tournament_url }
+    return render(request, 'hackapp/bracket.html', context)
