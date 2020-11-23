02ad96259553eed7418845f9b801fb6418ecfcc2
shanjng <shanjng@umich.edu>
2020-11-23 11:24:01 -0500

    working signup

--- hacksite/hackapp/urls.py
+++ hacksite/hackapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'hackapp' # in case we have another app that also has a path called signup, etc, to help differentiate, used in our index.html file when we do hackapp:signup to route
 urlpatterns = [
     path('', views.index, name="index"),
-    path('signup/', views.signup, name="signup"),
+    path('signup/<str:tournament_url>/', views.signup, name="signup"),
     path('bracket/<str:tournament_url>/', views.bracket, name="bracket"),
     path('create/', views.create, name="create")
 ]
