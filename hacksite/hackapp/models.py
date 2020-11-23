63c562c3495ec36b15611aec4de5ac466f84052a
Chinmay Savanur <csavanur@umich.edu>
2020-11-23 13:27:59 -0500

    Working on adding user sign up

--- hacksite/hackapp/models.py
+++ hacksite/hackapp/models.py
@@ -8,28 +8,28 @@ class Player(models.Model):
 	points = models.IntegerField(blank=True)
     # Player selects their pledge class
 	ELDON = 'Eldon'
-    OMICRON = 'Omicron'
-    PI = 'Pi'
-    RHO = 'Rho'
-    SIGMA = 'Sigma'
-    PLEDGE_CLASS_CHOICES = (
-        (ELDON, 'Eldon'),
-        (OMICRON, 'Omicron'),
-        (PI, 'Pi'),
-        (RHO, 'Rho'),
-        (SIGMA, 'Sigma'),
-    )
-    pledgeClass = models.TextField(
-        choices=PLEDGE_CLASS_CHOICES,
-        default=ELDON,
-    )
+	OMICRON = 'Omicron'
+	PI = 'Pi'
+	RHO = 'Rho'
+	SIGMA = 'Sigma'
+	PLEDGE_CLASS_CHOICES = (
+		(ELDON, 'Eldon'),
+		(OMICRON, 'Omicron'),
+		(PI, 'Pi'),
+		(RHO, 'Rho'),
+		(SIGMA, 'Sigma'),
+	)
+	pledgeClass = models.TextField(
+		choices=PLEDGE_CLASS_CHOICES,
+		default=ELDON,
+	)
 	
-class Leaderboard(models.Model):
+#class Leaderboard(models.Model):
     # Member variables to store the current players with the most and least accumulated points
-    firstPlace = models.ForeignKey(Player, null=True, blank=True, on_delete=models.CASCADE)
-    secondPlace = models.ForeignKey(Player, null=True, blank=True, on_delete=models.CASCADE)
-    thirdPlace = models.ForeignKey(Player, null=True, blank=True, on_delete=models.CASCADE)
-    lastPlace = models.ForeignKey(Player, null=True, blank=True, on_delete=models.CASCADE)
+    #firstPlace = models.ForeignKey(Player, null=True, blank=True, on_delete=models.CASCADE)
+    #secondPlace = models.ForeignKey(Player, null=True, blank=True, on_delete=models.CASCADE)
+    #thirdPlace = models.ForeignKey(Player, null=True, blank=True, on_delete=models.CASCADE)
+    #lastPlace = models.ForeignKey(Player, null=True, blank=True, on_delete=models.CASCADE)
 
-class Game(models.Model):
+#class Game(models.Model):
     # Insert code related to a game
--- hacksite/hackapp/views.py
+++ hacksite/hackapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 import requests
 
 # Create your views here.
@@ -33,4 +37,17 @@ def bracket(request, tournament_url):
 def create(request): 
     context = { "api_key": settings.API_KEY }
     return render(request, 'hackapp/create.html', context)
-    
+
+def usersignup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'usersignup.html', {'form': form})
