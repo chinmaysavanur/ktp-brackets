d1f7ea4c7f190ea81e91b1f6d4d4c82fcfbc79bb
shanjng <shanjng@umich.edu>
2020-11-24 11:55:40 -0500

    some styling

--- hacksite/hackapp/models.py
+++ hacksite/hackapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Tournament:
+    def __init__(self, name, id, url):
+        self.name = name
+        self.id = id
+        self.url = url
