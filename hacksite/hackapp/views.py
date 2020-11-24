63231780cc61fad9d838c8e1afec6df163123350
shanjng <shanjng@umich.edu>
2020-11-24 11:55:40 -0500

    some styling

--- hacksite/hackapp/views.py
+++ hacksite/hackapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
+from . import models
 import requests
+
+participant_id = 134634051
 
 # Create your views here.
 def index(request):
@@ -10,12 +13,13 @@ def index(request):
     tournamentsList = []
 
     for tournament in tournaments: 
-        tournamentsList.append({
-                "name": tournament['tournament']['name'],
-                "url": tournament['tournament']['url']
-            })
-
-    context = { 'data': tournamentsList }
+        tournamentsList.append(models.Tournament(
+            tournament['tournament']['name'], 
+            tournament['tournament']['id'], 
+            tournament['tournament']['url'])
+        )
+        
+    context = { 'tournaments': tournamentsList }
 
     return render(request, 'hackapp/index.html', context)
 
@@ -27,7 +31,21 @@ def signup(request, tournament_url):
     return render(request, 'hackapp/signup.html', context)
 
 def bracket(request, tournament_url): 
-    context = { "tournament_url": tournament_url }
+    state = 'open'
+
+    response = requests.get(f'https://api.challonge.com/v1/tournaments/{ tournament_url }/matches.json?api_key={ settings.API_KEY }&state={ state }&participant_id={ participant_id }')
+    matches = response.json()
+
+    matchesList = []
+
+    for match in matches:
+        matchesList.append(match['match'])
+
+    context = { 
+        "tournament_url": tournament_url, 
+        "participant_id": participant_id,
+        "matches": matchesList
+        }
     return render(request, 'hackapp/bracket.html', context)
 
 def create(request): 
