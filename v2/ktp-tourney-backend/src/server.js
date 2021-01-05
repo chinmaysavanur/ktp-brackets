4a3c92230408130b9f5026adbcf585c24daef86c
Chinmay Savanur <csavanur@umich.edu>
2021-01-05 17:12:51 -0500

    Added cors, mongoose, nodemon

--- v2/ktp-tourney-backend/src/server.js
+++ v2/ktp-tourney-backend/src/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => res.send('Hello!'));
