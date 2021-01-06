b635eda8590cb5d79e01e9cdb207a06ed380d090
shanjng <shanjng@umich.edu>
2021-01-06 11:08:28 -0500

    some frontend work

--- v2/ktp-brackets-frontend/src/components/Header.js
+++ v2/ktp-brackets-frontend/src/components/Header.js
@@ -1,17 +1,62 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from '../KTP_brackets_logo.png';
 import profile from '../Profile.png';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5em;
+  background-color: #c4c4c4;
+`;
+
+const Logo = styled.a`
+  margin-left: 1em;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+
+  margin: 0;
+  padding: 0;
+`;
+
+const StyledLink = styled(NavLink)`
+  display: inline-block;
+  font-size: 1.5rem;
+  background-color: black;
+  text-decoration: none;
+  color: inherit;
+  padding: 0.2em 0.5em;
+  margin: 0px 0.3em;
+  text-align: center;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+  &.active {
+    color: black;
+    background-color: white;
+    font-weight: bold;
+  }
+`;
 
 const Header = () => (
-    <nav className="navbar">
-        <a className="logo" href="#"><img src={logo} alt="logo"></img></a>
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Brackets</a></li>
-            <li><a href="#">Leaderboard</a></li>
-            {/* <li><a className="profile-link" href="#"><img src={profile} alt="logo"></img></a></li> */}
-        </ul>
-    </nav>
+  <Nav className='header'>
+    <Logo>
+      <img style={{ height: '100%' }} src={logo} alt='logo'></img>
+    </Logo>
+
+    <NavLinks>
+      <StyledLink exact to='/'>
+        Home
+      </StyledLink>
+      <StyledLink to='/brackets'>Brackets</StyledLink>
+      <StyledLink to='/leaderboard'>Leaderboard</StyledLink>
+    </NavLinks>
+  </Nav>
 );
 
 export default Header;
--- v2/ktp-brackets-frontend/src/pages/HomePage.js
+++ v2/ktp-brackets-frontend/src/pages/HomePage.js
@@ -1,30 +1,28 @@
 import React from 'react';
+import styles from './HomePage.module.css';
 
 const HomePage = () => {
-    return (
-        <div className="home">
-            <div>
-                <div>
-                    <h2>My Games</h2>
-                    <div class="game-thumbnail">
-                        Mini Gold Tournament - All
-                    </div>
-                </div>
-                <div>
-                    <h2>Active Games</h2>
-                    <div class="game-thumbnail">
-
-                    </div>
-                </div>
-            </div>
-            <div>
-                <h2>Game Updates</h2>
-                <div>
-
-                </div>
-            </div>
+  return (
+    <div className='home'>
+      <div className={styles.games}>
+        <div className={styles.myGamesContainer}>
+          <h2>My Games</h2>
+          <div className={styles.myGamesGameList}>
+            <div class={styles.gamePreview}>Mini Golf Tournament - All</div>
+          </div>
         </div>
-    );
-}
+        <div className={styles.activeGamesContainer}>
+          <h2>Active Games</h2>
+          <div class={styles.gamePreview}>8-Ball Pool Tournament</div>
+        </div>
+        {/* <div className='createGame'></div> */}
+      </div>
+      <div className={styles.gameUpdatesContainer}>
+        <h2>Game Updates</h2>
+        <div></div>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
