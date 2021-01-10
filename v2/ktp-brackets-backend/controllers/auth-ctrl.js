b4f8fa068f00824839df0d2684e927a53c5338b2
shanjng <shanjng@umich.edu>
2021-01-09 21:54:32 -0500

    login, register, jwt done

---
+++ v2/ktp-brackets-backend/controllers/auth-ctrl.js
@@ -0,0 +1,84 @@
+const User = require('../models/user-model');
+const { registerValidation, loginValidation } = require('../validation');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const saltRounds = 10; // complexity hash
+
+const createUser = async (req, res) => {
+  const body = req.body;
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: 'You must provide a user',
+    });
+  }
+
+  // validation
+  const { error, value } = await registerValidation(body);
+  if (error) {
+    const error_message = error.details[0].message;
+    return res.status(400).json({ error_message });
+  }
+
+  // check if user exists already via email
+  const emailExists = await User.findOne({ email: req.body.email });
+  if (emailExists) return res.status(400).send('Email already exists');
+
+  // hash password
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hashedPassword = await bcrypt.hash(body.password, salt);
+
+  body.password = hashedPassword;
+  const user = new User(body);
+
+  if (!user) {
+    return res.status(400).json({ success: false, error: err });
+  }
+
+  user
+    .save()
+    .then(() => {
+      return res.status(201).json({
+        success: true,
+        id: user._id,
+        message: 'User created!',
+      });
+    })
+    .catch((error) => {
+      return res.status(400).json({
+        error,
+        message: 'User not created!',
+      });
+    });
+};
+
+const loginUser = async (req, res) => {
+  const body = req.body;
+
+  const { error, value } = await loginValidation(body);
+  if (error) {
+    const error_message = error.details[0].message;
+    return res.status(400).json({ error: error_message });
+  }
+
+  // check if email does not exist
+  const user = await User.findOne({ email: body.email });
+  if (!user) return res.status(400).json({ error: 'Email is not found' });
+
+  // replace error message eventually with 'Email or password is wrong' for privacy
+
+  // check if passowrd is correct
+  const validPass = await bcrypt.compare(body.password, user.password);
+  if (!validPass) return res.status(400).send({ error: 'Invalid password' });
+
+  // Create JWT with user id and assign a token
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+  res.header('auth-token', token).send(token);
+};
+
+module.exports = {
+  createUser,
+  loginUser,
+};
--- v2/ktp-brackets-backend/models/user-model.js
+++ v2/ktp-brackets-backend/models/user-model.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const User = new Schema(
-    {
-        name: { type: String, required: true },
-        email: { type: String, required: true},
-        pledgeClass: { type : String, required: true },
-    },
-    { timestamps: true },
-)
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    pledgeClass: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('users', User);
+module.exports = mongoose.model('user', User);
