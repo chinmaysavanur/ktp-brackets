eda0452a88b371520790bfdde8329afad08e101c
Chinmay Savanur <csavanur@umich.edu>
2021-01-17 16:08:05 -0500

    Create routes for frontend

--- v2/ktp-brackets-backend/routes/tournament-router.js
+++ v2/ktp-brackets-backend/routes/tournament-router.js
@@ -4,4 +4,10 @@ const TournamentCtrl = require('../controllers/tournament-ctrl');
 
 const router = express.Router();
 
+router.post('/tournament', TournamentCtrl.createTournament)
+router.put('/tournament/:id', TournamentCtrl.updateTournament)
+router.delete('/tournament/:id', TournamentCtrl.deleteTournament)
+router.get('/tournament/:id', TournamentCtrl.getTournamentById)
+router.get('/tournaments', TournamentCtrl.getTournaments)
+
 module.exports = router;
--- v2/ktp-brackets-backend/routes/userTournament-router.js
+++ v2/ktp-brackets-backend/routes/userTournament-router.js
@@ -1,7 +1,11 @@
-const express = require('express');
+﻿const express = require('express');
 
 const UserTournamentCtrl = require('../controllers/userTournament-ctrl');
 
 const router = express.Router();
+
+router.post('/userTournament', UserTournamentCtrl.createUserTournament)
+router.get('/tournamentsByUser/:id', UserTournamentCtrl.getTournamentsByUser)
+router.get('/usersByTournament/:id', UserTournamentCtrl.getUsersByTournament)
 
 module.exports = router;
